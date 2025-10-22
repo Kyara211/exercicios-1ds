d430f2b0c57e0482600d780a4ae09522d533d4f4
kyara <angelakyara2021@gmail.com>
2025-10-22 00:34:56 +0000

    terminado

--- 4-bimestre/projetos/projeto5-sistema-de-controle-de-producao/index.js
+++ 4-bimestre/projetos/projeto5-sistema-de-controle-de-producao/index.js
@@ -2,3 +2,36 @@
 // Comando: npm i prompt-sync; npm i -D jest
 
 // CRIE UMA LÓGICA ABAIXO
+const prompt = require("prompt-sync")()
+
+let diaAtual = 1
+let continuar = ""
+let acumulado = 0
+let historico = ""
+
+do {
+  let metaTurno = parseInt(prompt("Quantas peças devem ser produzidas no turno? "))
+  let producao = 0
+
+  for (let i = 1; i <= metaTurno; i++) {
+    producao = i
+    console.log(`Peça ${i} produzida com sucesso.`)
+  }
+
+  acumulado += producao
+  console.log(`Dia ${diaAtual}: ${producao} peças produzidas.`)
+
+  if(producao == metaTurno){
+    console.log("Meta alcançada!")
+  }else{
+    console.log("Meta não alcançada!")
+  }
+  historico += `Dia ${diaAtual}: ${producao} peças produzidas\n`
+  diaAtual++
+
+  continuar = prompt("Deseja simular outro turno? (S/N) ").toLowerCase()
+} while (continuar === "s")
+console.log("Resumo dos dias anteriores:")
+console.log(historico)
+console.log(`Total geral: ${acumulado}`)
+console.log("Encerrando sistema de produção...")
